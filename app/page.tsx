05847a29b94e4d86ba5240bca1377b2bc751fa2a
agarciaa99 <aega2399@outlook.com>
2026-02-23 11:34:04 -0600

    Carousel del hero terminado

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/layout/Header";
+import HeroCarousel from "@/components/home/HeroCarousel";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
       <Header />
+      <HeroCarousel />
     </div>
   );
 }
