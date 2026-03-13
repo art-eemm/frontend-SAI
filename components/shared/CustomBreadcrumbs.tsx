b411b80122a8f1f4f648d86e3794bfcd7a971713
agarciaa99 <aega2399@outlook.com>
2026-03-13 10:12:19 -0600

    Corrección en los breacrumbs de el dashboard

--- components/shared/CustomBreadcrumbs.tsx
+++ components/shared/CustomBreadcrumbs.tsx
@@ -15,21 +15,27 @@ import { getFormattedName } from "@/lib/utils";
 export function CustomBreadcrumbs() {
   const pathname = usePathname();
   const segments = pathname.split("/").filter((item) => item !== "");
+  const isDashboard = segments[0] === "dashboard";
 
   return (
     <Breadcrumb className="mb-6">
       <BreadcrumbList>
-        <BreadcrumbItem>
-          <BreadcrumbLink href="/">Inicio</BreadcrumbLink>
-        </BreadcrumbItem>
+        {!isDashboard && (
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Inicio</BreadcrumbLink>
+          </BreadcrumbItem>
+        )}
 
         {segments.map((segment, index) => {
           const href = `/${segments.slice(0, index + 1).join("/")}`;
           const isLast = index === segments.length - 1;
 
+          const showSeparator = !isDashboard || index > 0;
+
           return (
             <React.Fragment key={href}>
-              <BreadcrumbSeparator />
+              {showSeparator && <BreadcrumbSeparator />}
+
               <BreadcrumbItem>
                 {isLast ? (
                   <BreadcrumbPage className="capitalize">
